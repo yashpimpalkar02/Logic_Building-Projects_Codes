7032415a56a4fac0b001f29f0b1eccd4422c8e16
Yash Pimpalkar <yashpimpalkar999@gmail.com>
2024-11-14 12:54:15 +0530

    Program 115
    Write a program to find ASCII Value of the character.

---
+++ Program115.c
@@ -0,0 +1,15 @@
+// To find ASCII value of the character
+
+#include<stdio.h>
+
+int main()
+{
+    char ch = '\0';
+
+    printf("Enter the character : \n");
+    scanf("%c",&ch);
+
+    printf("ASCII value of the character is : %d\n",ch);
+
+    return 0;
+}
