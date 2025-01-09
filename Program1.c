6c84fc403fa171af18ff3e05c5164be81c057b56
Yash <yashpimpalkar999@gmail.com>
2025-01-09 22:15:05 +0530

    Removed program files

--- Program1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    printf("Hello Yamini...\n");
-
-    return 0;
-}
--- Program236.java
+++
@@ -1,8 +0,0 @@
-
-class Program236
-{
-    public static void main(String Arg[])
-    {
-        System.out.println("Jay Ganesh..");
-    }
-}
